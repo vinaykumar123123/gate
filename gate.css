e69dc5cd4363869d012fa6fac8ce2801c35db452
vinaykumar123123 <142776619+vinaykumar123123@users.noreply.github.com>
2023-09-02 12:25:05 +0530

    Add files via upload

---
+++ gate.css
@@ -0,0 +1,11 @@
+button{
+    color: red;
+    background-color: white;
+    height: 50px;
+    width: 120px;
+    border-radius: 20%
+}
+button:hover{
+    color:aliceblue;
+    cursor: pointer;
+}
---
+++ gate.js
@@ -0,0 +1,6 @@
+function gopen(){
+    document.getElementById('image').src="opengate.png";
+    }
+       function gclose(){
+    document.getElementById('image').src ="closegate.png";
+}
